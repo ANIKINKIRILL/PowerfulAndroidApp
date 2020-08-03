66204d8323d2d44afa31fcc0994395ddd1d109b1
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-03 10:57:29 +0300

    Starting Point

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/AuthActivity.kt
@@ -0,0 +1,11 @@
+package com.anikinkirill.powerfulandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
