ce7de1ed739b9f5c354e6bc44c67bf7a39559aef
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-03 16:18:52 +0300

    Navigation Graph for Authentication Screen

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/LauncherFragment.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/LauncherFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.anikinkirill.powerfulandroidapp.R
+import kotlinx.android.synthetic.main.fragment_launcher.*
 
 class LauncherFragment : Fragment() {
 
@@ -15,6 +17,37 @@ class LauncherFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login.setOnClickListener {
+            navLogin()
+        }
+
+        register.setOnClickListener {
+            navRegister()
+        }
+
+        forgot_password.setOnClickListener {
+            navForgotPassword()
+        }
+
+        focusable_view.requestFocus()
+
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+
+    private fun navRegister() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
     }
 
 }
