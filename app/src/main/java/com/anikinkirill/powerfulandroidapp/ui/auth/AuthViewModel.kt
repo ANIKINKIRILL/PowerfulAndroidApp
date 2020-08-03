c6ea2640589df8ce2b96f9bb74f9a8bec46f1264
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-03 20:12:58 +0300

    AuthViewModel @Inject annotation

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/AuthViewModel.kt
@@ -2,7 +2,8 @@ package com.anikinkirill.powerfulandroidapp.ui.auth
 
 import androidx.lifecycle.ViewModel
 import com.anikinkirill.powerfulandroidapp.repository.auth.AuthRepository
+import javax.inject.Inject
 
-class AuthViewModel constructor(val authRepository: AuthRepository) : ViewModel() {
+class AuthViewModel @Inject constructor(val authRepository: AuthRepository) : ViewModel() {
 
 }
