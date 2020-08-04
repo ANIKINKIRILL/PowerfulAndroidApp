cab865492212d6550dadeb66d5e65aebbb99e25a
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-04 15:53:59 +0300

    Add login and register functions to AuthService

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/api/auth/OpenApiAuthService.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/api/auth/OpenApiAuthService.kt
@@ -1,5 +1,27 @@
 package com.anikinkirill.powerfulandroidapp.api.auth
 
+import androidx.lifecycle.LiveData
+import com.anikinkirill.powerfulandroidapp.util.ApiResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface OpenApiAuthService {
+
+    @POST("api/account/login")
+    @FormUrlEncoded
+    fun login(
+        @Field("username") email: String,
+        @Field("password") password: String
+    ) : LiveData<ApiResponse<LoginResponse>>
+
+    @POST("api/account/register")
+    @FormUrlEncoded
+    fun register(
+        @Field("email") email: String,
+        @Field("username") username: String,
+        @Field("password") password: String,
+        @Field("password2") password2: String
+    ) : LiveData<ApiResponse<RegistrationResponse>>
 
 }
