813529c7710413f93a42fb7179d8896c9d32099f
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-05 10:46:40 +0300

    Add LoginFields into AuthViewState

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/state/AuthViewState.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/state/AuthViewState.kt
@@ -24,6 +24,26 @@ data class RegistrationFields(
         }
         return RegistrationError.none()
     }
+}
 
+data class LoginFields(
+    var login_email: String? = null,
+    var login_password: String? = null
+){
+
+    class LoginError() {
+        companion object {
+            fun mustFillAllFields() = "All fields are required!"
+            fun none() = "None"
+        }
+    }
+
+    fun isValidForLogin() : String {
+        if(login_email.isNullOrEmpty() || login_password.isNullOrEmpty()) {
+            return LoginError.mustFillAllFields()
+        }
+        return LoginError.none()
+    }
 
 }
+
