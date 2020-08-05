8ba5ae4776d9e84e63d0dbefa3743234a6d8ad7b
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-05 09:50:50 +0300

    Create AuthStateEvent class

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/state/AuthStateEvent.kt
@@ -0,0 +1,20 @@
+package com.anikinkirill.powerfulandroidapp.ui.auth.state
+
+sealed class AuthStateEvent {
+
+    data class LoginAttemptEvent(
+        val email: String,
+        val password: String
+    ) : AuthStateEvent()
+
+    data class RegisterAttemptEvent(
+        val email: String,
+        val username: String,
+        val password: String,
+        val confirm_password: String
+    ) : AuthStateEvent()
+
+    class CheckPreviousAuthEvent() : AuthStateEvent()
+
+}
+
