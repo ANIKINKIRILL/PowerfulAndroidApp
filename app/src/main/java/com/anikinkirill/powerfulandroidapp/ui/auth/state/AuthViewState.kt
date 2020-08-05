20bc247f4026681447aa0e8641401caacb20075c
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-05 10:25:52 +0300

    Add RegistrationFields into AuthViewState

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/state/AuthViewState.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/state/AuthViewState.kt
@@ -1,2 +1,29 @@
 package com.anikinkirill.powerfulandroidapp.ui.auth.state
 
+data class RegistrationFields(
+    var registration_email: String? = null,
+    var registration_username: String? = null,
+    var registration_password: String? = null,
+    var registration_confirm_password: String? = null
+) {
+
+    class RegistrationError() {
+        companion object {
+            fun mustFillAllFields() = "All fields are required!"
+            fun passwordDoNotMatch() = "Password must match!"
+            fun none() = "None"
+        }
+    }
+
+    fun isValidForRegistration() : String {
+        if(registration_password != registration_confirm_password) {
+            return RegistrationError.passwordDoNotMatch()
+        }
+        if(registration_email.isNullOrEmpty() || registration_username.isNullOrEmpty() || registration_password.isNullOrEmpty() || registration_confirm_password.isNullOrEmpty()) {
+            return RegistrationError.mustFillAllFields()
+        }
+        return RegistrationError.none()
+    }
+
+
+}
