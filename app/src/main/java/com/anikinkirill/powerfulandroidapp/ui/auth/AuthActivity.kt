f2a027013730a049b760f995c48f95b25eed0a58
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-08 15:19:54 +0300

    Update AuthActivity
    Apply login logic to a AuthActivity

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/AuthActivity.kt
@@ -1,9 +1,12 @@
 package com.anikinkirill.powerfulandroidapp.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.anikinkirill.powerfulandroidapp.R
 import com.anikinkirill.powerfulandroidapp.ui.BaseActivity
+import com.anikinkirill.powerfulandroidapp.ui.main.MainActivity
 import com.anikinkirill.powerfulandroidapp.viewmodels.ViewModelProviderFactory
 import javax.inject.Inject
 
@@ -19,5 +22,28 @@ class AuthActivity : BaseActivity() {
         setContentView(R.layout.activity_auth)
 
         authViewModel = ViewModelProvider(this, providerFactory).get(AuthViewModel::class.java)
+
+        subscribeObservers()
     }
+
+    private fun subscribeObservers() {
+
+        authViewModel.viewState.observe(this, Observer { authViewState ->
+            authViewState.authToken?.let { authToken ->
+                sessionManager.login(authToken)
+            }
+        })
+
+        sessionManager.cachedToken.observe(this, Observer { authToken ->
+            if(authToken != null && authToken.account_pk != -1 && authToken.token != null) {
+                navMainActivity()
+            }
+        })
+    }
+
+    private fun navMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
 }
