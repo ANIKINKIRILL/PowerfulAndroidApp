da8cf948a0a261e28b322ba25c4dfbce7d24844d
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-08 14:30:02 +0300

    Inject SessionManager to BaseActivity

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/BaseActivity.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/BaseActivity.kt
@@ -1,8 +1,13 @@
 package com.anikinkirill.powerfulandroidapp.ui
 
+import com.anikinkirill.powerfulandroidapp.session.SessionManager
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var sessionManager: SessionManager
 
     companion object {
         const val TAG = "BaseActivity"
