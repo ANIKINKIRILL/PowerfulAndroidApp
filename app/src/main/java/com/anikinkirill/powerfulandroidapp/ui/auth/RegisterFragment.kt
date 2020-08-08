cbd2bfba73fbf2a484cac45b2e757889e95f765a
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-08 17:57:44 +0300

    Save input data in RegisterFragment

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/auth/RegisterFragment.kt
@@ -1,11 +1,13 @@
 package com.anikinkirill.powerfulandroidapp.ui.auth
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.anikinkirill.powerfulandroidapp.R
+import com.anikinkirill.powerfulandroidapp.ui.auth.state.RegistrationFields
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : BaseAuthFragment() {
 
@@ -19,8 +21,30 @@ class RegisterFragment : BaseAuthFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        subscribeObservers()
+    }
 
-        Log.d(TAG, "onViewCreated: RegisterFragment: ${authViewModel.hashCode()}")
+    private fun subscribeObservers() {
+        authViewModel.viewState.observe(this, Observer { authViewState ->
+            authViewState.registrationFields?.let { registrationFields ->
+                registrationFields.registration_email?.let { input_email.setText(it) }
+                registrationFields.registration_username?.let { input_username.setText(it) }
+                registrationFields.registration_password?.let { input_password.setText(it) }
+                registrationFields.registration_confirm_password?.let { input_password_confirm.setText(it) }
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        authViewModel.setRegistrationFields(
+            RegistrationFields(
+                input_email.text.toString(),
+                input_username.text.toString(),
+                input_password.text.toString(),
+                input_password_confirm.text.toString()
+            )
+        )
     }
 
 }
