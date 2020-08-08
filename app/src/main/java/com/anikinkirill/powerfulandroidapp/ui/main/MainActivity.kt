7685546c3099d97762b5c843d517bf1302c35c83
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-08 14:21:34 +0300

    Add MainActivity.kt

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.anikinkirill.powerfulandroidapp.ui.main
+
+import android.os.Bundle
+import com.anikinkirill.powerfulandroidapp.R
+import com.anikinkirill.powerfulandroidapp.ui.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
