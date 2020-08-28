3299b1308613199e85a8955fa80d2fc086acf948
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-28 15:45:16 +0300

    Add consts to Constants class

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/util/Constants.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/util/Constants.kt
@@ -4,6 +4,10 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://open-api.xyz/"
+
+        const val NETWORK_TIMEOUT = 3000L
+        const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
+        const val TESTING_CACHE_DELAY = 0L // fake cache delay for testing
     }
 
 }
