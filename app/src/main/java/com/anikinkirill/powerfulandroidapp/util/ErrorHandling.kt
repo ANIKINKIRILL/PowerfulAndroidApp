2f48a5fbf634f3fd00a60fd07c543378d62af201
ANIKINKIRILL <ky.anikin@mail.ru>
2020-08-28 15:44:00 +0300

    Add consts to ErrorHandling class

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/util/ErrorHandling.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/util/ErrorHandling.kt
@@ -3,7 +3,18 @@ package com.anikinkirill.powerfulandroidapp.util
 class ErrorHandling {
 
     companion object {
-        const val UNKNOWN_ERROR = "Unknown error"
+        const val UNABLE_TO_RESOLVE_HOST = "Unable to resolve host"
+        const val UNABLE_TODO_OPERATION_WO_INTERNET = "Can't do that operation without an internet connection"
+        const val ERROR_CHECK_NETWORK_CONNECTION = "Check network connection."
+
+        const val ERROR_UNKNOWN = "Unknown error"
+
+        fun isNetworkError(msg: String): Boolean{
+            return when {
+                msg.contains(UNABLE_TO_RESOLVE_HOST) -> true
+                else -> false
+            }
+        }
     }
 
 }
