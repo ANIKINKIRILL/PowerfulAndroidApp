ad28f0ea6adbf2572f99fb44dbba428e7eb22d18
ANIKINKIRILL <ky.anikin@mail.ru>
2020-09-08 18:10:04 +0300

    Add searchByPk function in AuthTokenDao

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/AuthTokenDao.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/AuthTokenDao.kt
@@ -15,4 +15,7 @@ interface AuthTokenDao {
     @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
     fun nullifyToken(pk: Int) : Int
 
+    @Query("SELECT * FROM auth_token WHERE account_pk = :pk")
+    suspend fun searchByPk(pk: Int) : AuthToken?
+
 }
