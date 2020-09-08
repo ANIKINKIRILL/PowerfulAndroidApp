3653c999a0e0134f22c90c40dcfab52b9942bfc7
ANIKINKIRILL <ky.anikin@mail.ru>
2020-09-08 18:05:36 +0300

    Add PreferenceKeys utility class
    This class for working with SharedPreferences

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/util/PreferenceKeys.kt
@@ -0,0 +1,17 @@
+package com.anikinkirill.powerfulandroidapp.util
+
+class PreferenceKeys {
+
+    companion object{
+
+        // Shared Preference Files:
+        const val APP_PREFERENCES: String = "com.anikinkirill.powerfulandroidapp.APP_PREFERENCES"
+
+        // Shared Preference Keys
+        const val PREVIOUS_AUTH_USER: String = "com.anikinkirill.powerfulandroidapp.PREVIOUS_AUTH_USER"
+        const val BLOG_FILTER: String = "com.anikinkirill.powerfulandroidapp.BLOG_FILTER"
+        const val BLOG_ORDER: String = "com.anikinkirill.powerfulandroidapp.BLOG_ORDER"
+
+    }
+
+}
