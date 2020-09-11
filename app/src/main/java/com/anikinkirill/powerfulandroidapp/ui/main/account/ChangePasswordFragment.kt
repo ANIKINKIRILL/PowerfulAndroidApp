4ef1b9ef1670109baffbe02918f4be7d1f41a4ff
ANIKINKIRILL <ky.anikin@mail.ru>
2020-09-11 16:50:20 +0300

    Create ChangePasswordFragment

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/account/ChangePasswordFragment.kt
@@ -0,0 +1,23 @@
+package com.anikinkirill.powerfulandroidapp.ui.main.account
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.anikinkirill.powerfulandroidapp.R
+
+class ChangePasswordFragment : BaseAccountFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_change_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
