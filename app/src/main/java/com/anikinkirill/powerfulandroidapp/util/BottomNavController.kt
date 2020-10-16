a83b74992c924d1ff773192050d9a20206e37f78
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-16 10:52:34 +0300

    Add OnNavigationItemChanged
    For highlighting selected item in bottom nav menu

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/util/BottomNavController.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/util/BottomNavController.kt
@@ -1,2 +1,17 @@
 package com.anikinkirill.powerfulandroidapp.util
 
+class BottomNavController() {
+
+    /**
+     *  For highlighting the selected item
+     *  in the BottomNavigationMenu
+     *  Example: from HOME user clicks CREATE
+     *  So CREATE item is going to be highlighted (blue color)
+     *
+     */
+    interface OnNavigationItemChanged {
+        // itemId - R.menu.menu_nav_blog, R.menu.menu_nav_create_blog, R.menu.menu_nav_account
+        fun onItemChange(itemId: Int)
+    }
+
+}
