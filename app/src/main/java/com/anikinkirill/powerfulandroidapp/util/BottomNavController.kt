d6ad50be84408455bb7f8a1725e626b1196a0186
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-16 11:08:24 +0300

    Add NavGraphProvider
    Get the graph id from the item id in bottom nav menu

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/util/BottomNavController.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/util/BottomNavController.kt
@@ -1,5 +1,7 @@
 package com.anikinkirill.powerfulandroidapp.util
 
+import androidx.annotation.NavigationRes
+
 class BottomNavController() {
 
     /**
@@ -10,8 +12,26 @@ class BottomNavController() {
      *
      */
     interface OnNavigationItemChanged {
-        // itemId - R.menu.menu_nav_blog, R.menu.menu_nav_create_blog, R.menu.menu_nav_account
+        /**
+         * @param itemId - R.menu.menu_nav_blog, R.menu.menu_nav_create_blog, R.menu.menu_nav_account
+         */
         fun onItemChange(itemId: Int)
+    }
+
+    /**
+     * Get navigation graph id
+     * @NavigationRes means that returned type of
+     * getNavGraphId is going to be navigation resource reference
+     * Example: R.navigation.nav_blog
+     */
+
+    interface NavGraphProvider {
+        /**
+         * @param itemId - R.menu.menu_nav_blog, R.menu.menu_nav_create_blog, R.menu.menu_nav_account
+         * @return navigation resource reference (e.g R.navigation.nav_blog)
+         */
+        @NavigationRes
+        fun getNavGraphId(itemId: Int) : Int
     }
 
 }
