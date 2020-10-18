8ccb6e2fc98b2ca8a67776afd84595e1e3362014
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-18 12:36:05 +0300

    Setup BlogFragment

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.anikinkirill.powerfulandroidapp.R
+import kotlinx.android.synthetic.main.fragment_blog.*
 
 class BlogFragment : BaseBlogFragment() {
 
@@ -18,6 +20,11 @@ class BlogFragment : BaseBlogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        goViewBlogFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
+        }
+
     }
 
 }
