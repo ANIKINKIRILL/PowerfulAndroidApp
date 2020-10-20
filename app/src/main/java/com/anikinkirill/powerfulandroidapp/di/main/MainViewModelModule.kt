688da4edbbc9cfb9b1527160f4c5baeeac9be610
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-20 10:49:40 +0300

    Setup Dagger for MainViewModelModule
    Provide AccountViewModel

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/di/main/MainViewModelModule.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/di/main/MainViewModelModule.kt
@@ -1,8 +1,18 @@
 package com.anikinkirill.powerfulandroidapp.di.main
 
+import androidx.lifecycle.ViewModel
+import com.anikinkirill.powerfulandroidapp.di.ViewModelKey
+import com.anikinkirill.powerfulandroidapp.ui.main.account.AccountViewModel
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class MainViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AccountViewModel::class)
+    abstract fun bindAccountViewModel(accountViewModel: AccountViewModel) : ViewModel
 
 }
