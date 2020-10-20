466b79682f3f1c5690742bc20ed7409350c4bda1
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-20 10:00:30 +0300

    Add AccountViewState

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/account/state/AccountViewState.kt
@@ -0,0 +1,7 @@
+package com.anikinkirill.powerfulandroidapp.ui.main.account.state
+
+import com.anikinkirill.powerfulandroidapp.models.AccountProperties
+
+class AccountViewState(
+    var accountProperties: AccountProperties? = null
+)
