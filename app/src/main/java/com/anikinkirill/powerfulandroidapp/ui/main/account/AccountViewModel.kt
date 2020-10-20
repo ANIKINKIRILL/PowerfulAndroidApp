063294c4f58dae8787fbeead7f34c0f7309ed0b6
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-20 10:40:25 +0300

    Create AccountViewModel

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/account/AccountViewModel.kt
@@ -0,0 +1,56 @@
+package com.anikinkirill.powerfulandroidapp.ui.main.account
+
+import androidx.lifecycle.LiveData
+import com.anikinkirill.powerfulandroidapp.models.AccountProperties
+import com.anikinkirill.powerfulandroidapp.repository.main.AccountRepository
+import com.anikinkirill.powerfulandroidapp.session.SessionManager
+import com.anikinkirill.powerfulandroidapp.ui.BaseViewModel
+import com.anikinkirill.powerfulandroidapp.ui.DataState
+import com.anikinkirill.powerfulandroidapp.ui.main.account.state.AccountStateEvent
+import com.anikinkirill.powerfulandroidapp.ui.main.account.state.AccountStateEvent.*
+import com.anikinkirill.powerfulandroidapp.ui.main.account.state.AccountViewState
+import com.anikinkirill.powerfulandroidapp.util.AbsentLiveData
+import javax.inject.Inject
+
+class AccountViewModel
+@Inject constructor(
+    private val sessionManager: SessionManager,
+    private val accountRepository: AccountRepository
+): BaseViewModel<AccountStateEvent, AccountViewState>() {
+
+    override fun initNewViewState(): AccountViewState {
+        return AccountViewState()
+    }
+
+    override fun handleStateEvent(event: AccountStateEvent): LiveData<DataState<AccountViewState>> {
+        when(event) {
+            is GetAccountPropertiesEvent -> {
+                return AbsentLiveData.create()
+            }
+            is ChangePasswordEvent -> {
+                return AbsentLiveData.create()
+            }
+            is UpdateAccountPropertiesEvent -> {
+                return AbsentLiveData.create()
+            }
+            is None -> {
+                return AbsentLiveData.create()
+            }
+        }
+    }
+
+    fun setAccountProperties(accountProperties: AccountProperties) {
+        val update = getCurrentViewStateOrNew()
+        if(update.accountProperties == accountProperties) {
+            return
+        }
+        update.accountProperties = accountProperties
+        _viewState.value = update
+    }
+
+    fun logout() {
+        sessionManager.logout()
+    }
+
+
+}
