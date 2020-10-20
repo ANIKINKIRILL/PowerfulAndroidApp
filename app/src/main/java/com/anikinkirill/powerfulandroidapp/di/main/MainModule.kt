79af2275d6bee53368394dc56a8a100cf79407a0
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-20 10:47:43 +0300

    Setup Dagger for MainModule
    Provide OpenApiMainService and AccountRepository

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/di/main/MainModule.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/di/main/MainModule.kt
@@ -1,8 +1,30 @@
 package com.anikinkirill.powerfulandroidapp.di.main
 
+import com.anikinkirill.powerfulandroidapp.api.main.OpenApiMainService
+import com.anikinkirill.powerfulandroidapp.persitence.AccountPropertiesDao
+import com.anikinkirill.powerfulandroidapp.repository.main.AccountRepository
+import com.anikinkirill.powerfulandroidapp.session.SessionManager
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
 
 @Module
 class MainModule {
+
+    @MainScope
+    @Provides
+    fun provideOpenApiMainService(retrofit: Retrofit) : OpenApiMainService {
+        return retrofit.create(OpenApiMainService::class.java)
+    }
+
+    @MainScope
+    @Provides
+    fun provideAccountRepository(
+        openApiMainService: OpenApiMainService,
+        accountPropertiesDao: AccountPropertiesDao,
+        sessionManager: SessionManager
+    ) : AccountRepository {
+        return AccountRepository(openApiMainService, accountPropertiesDao, sessionManager)
+    }
 
 }
