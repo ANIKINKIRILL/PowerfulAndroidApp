4b7d06778a1669c514730b5d2b07fc66233c6433
ANIKINKIRILL <ky.anikin@mail.ru>
2020-10-20 10:06:40 +0300

    Add AccountStateEvent

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/account/state/AccountStateEvent.kt
@@ -0,0 +1,20 @@
+package com.anikinkirill.powerfulandroidapp.ui.main.account.state
+
+sealed class AccountStateEvent {
+
+    class GetAccountPropertiesEvent : AccountStateEvent()
+
+    data class UpdateAccountPropertiesEvent(
+        val email: String,
+        val username: String
+    ) : AccountStateEvent()
+
+    data class ChangePasswordEvent(
+        val currentPassword: String,
+        val newPassword: String,
+        val confirmNewPassword: String
+    ) : AccountStateEvent()
+
+    class None : AccountStateEvent()
+
+}
