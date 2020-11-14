481adb6efa700b8c45cde19e9c9dc9e95f25e3c8
ANIKINKIRILL <ky.anikin@mail.ru>
2020-11-14 14:08:25 +0300

    Update AccountRepository
    Add getAccountProperties function

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/repository/main/AccountRepository.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/repository/main/AccountRepository.kt
@@ -2,9 +2,16 @@ package com.anikinkirill.powerfulandroidapp.repository.main
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.lifecycle.LiveData
 import com.anikinkirill.powerfulandroidapp.api.main.OpenApiMainService
+import com.anikinkirill.powerfulandroidapp.models.AccountProperties
+import com.anikinkirill.powerfulandroidapp.models.AuthToken
 import com.anikinkirill.powerfulandroidapp.persitence.AccountPropertiesDao
+import com.anikinkirill.powerfulandroidapp.repository.NetworkBoundResource
 import com.anikinkirill.powerfulandroidapp.session.SessionManager
+import com.anikinkirill.powerfulandroidapp.ui.DataState
+import com.anikinkirill.powerfulandroidapp.ui.main.account.state.AccountViewState
+import com.anikinkirill.powerfulandroidapp.util.ApiResponse
 import kotlinx.coroutines.Job
 import javax.inject.Inject
 
@@ -21,6 +28,33 @@ class AccountRepository
     }
 
     private var repositoryJob: Job? = null
+
+    fun getAccountProperties(authToken: AuthToken): LiveData<DataState<AccountViewState>> {
+        return object : NetworkBoundResource<AccountProperties, AccountViewState>(
+            sessionManager.isConnectedToTheInternet(),
+            true
+        ) {
+            override suspend fun createCacheRequestAndReturn() {
+                TODO("Not yet implemented")
+            }
+
+            override suspend fun handleApiSuccessResponse(response: ApiResponse.ApiSuccessResponse<AccountProperties>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun createCall(): LiveData<ApiResponse<AccountProperties>> {
+                return openApiMainService.getAccountProperties(
+                    "Token ${authToken.token}"
+                )
+            }
+
+            override fun setJob(job: Job) {
+                repositoryJob?.cancel()
+                repositoryJob = job
+            }
+
+        }.asLiveData()
+    }
 
     fun cancelActiveJobs() {
         Log.d(TAG, "cancelActiveJobs: called")
