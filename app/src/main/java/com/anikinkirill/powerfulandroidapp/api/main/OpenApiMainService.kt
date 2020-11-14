3736508a72ec4d1ef92398d6e864dd654b42c010
ANIKINKIRILL <ky.anikin@mail.ru>
2020-11-14 13:44:50 +0300

    Update OpenApiMainService for Retrfoit
    Add getAccountProperties function

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/api/main/OpenApiMainService.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/api/main/OpenApiMainService.kt
@@ -1,7 +1,16 @@
 package com.anikinkirill.powerfulandroidapp.api.main
 
+import androidx.lifecycle.LiveData
+import com.anikinkirill.powerfulandroidapp.models.AccountProperties
+import com.anikinkirill.powerfulandroidapp.util.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+
 interface OpenApiMainService {
 
-
+    @GET("account/properties")
+    fun getAccountProperties(
+        @Header("Authorization") authorization: String
+    ): LiveData<ApiResponse<AccountProperties>>
 
 }
