112040577a6584a1fa1b2b0cb325f9910b5160e2
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-12 01:29:48 +0300

    Add BlogViewModel

---
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogViewModel.kt
@@ -0,0 +1,64 @@
+package com.anikinkirill.powerfulandroidapp.ui.main.blog
+
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import com.anikinkirill.powerfulandroidapp.models.BlogPost
+import com.anikinkirill.powerfulandroidapp.repository.main.BlogRepository
+import com.anikinkirill.powerfulandroidapp.session.SessionManager
+import com.anikinkirill.powerfulandroidapp.ui.BaseViewModel
+import com.anikinkirill.powerfulandroidapp.ui.DataState
+import com.anikinkirill.powerfulandroidapp.ui.main.blog.state.BlogStateEvent
+import com.anikinkirill.powerfulandroidapp.ui.main.blog.state.BlogStateEvent.*
+import com.anikinkirill.powerfulandroidapp.ui.main.blog.state.BlogViewState
+import com.anikinkirill.powerfulandroidapp.util.AbsentLiveData
+import javax.inject.Inject
+
+class BlogViewModel
+@Inject constructor(
+    private val sessionManager: SessionManager,
+    private val blogPostRepository: BlogRepository,
+    private val sharedPreferences: SharedPreferences,
+    private val requestManager: RequestManager
+) : BaseViewModel<BlogStateEvent, BlogViewState>() {
+
+    override fun initNewViewState(): BlogViewState {
+        return BlogViewState()
+    }
+
+    override fun handleStateEvent(event: BlogStateEvent): LiveData<DataState<BlogViewState>> {
+        return when (event) {
+            is BlogSearchEvent -> AbsentLiveData.create()
+            is None -> AbsentLiveData.create()
+        }
+    }
+
+    fun setQuery(query: String) {
+        val update = getCurrentViewStateOrNew()
+        if (query == update.blogFields.searchQuery) {
+            return
+        }
+        update.blogFields.searchQuery = query
+        _viewState.value = update
+    }
+
+    fun setBlogPostList(blogList: List<BlogPost>) {
+        val update = getCurrentViewStateOrNew()
+        update.blogFields.blogList = blogList
+        _viewState.value = update
+    }
+
+    private fun cancelActiveJobs() {
+        blogPostRepository.cancelActiveJobs()
+        handlePendingData()
+    }
+
+    private fun handlePendingData() {
+        setStateEvent(None())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        cancelActiveJobs()
+    }
+
+}
