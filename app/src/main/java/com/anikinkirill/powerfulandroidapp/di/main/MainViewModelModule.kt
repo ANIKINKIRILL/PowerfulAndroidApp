415d3ef6b869b5583031736cc265763dcc9d057f
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-12 01:37:06 +0300

    Provide BlogViewModel

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/di/main/MainViewModelModule.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/di/main/MainViewModelModule.kt
@@ -3,6 +3,7 @@ package com.anikinkirill.powerfulandroidapp.di.main
 import androidx.lifecycle.ViewModel
 import com.anikinkirill.powerfulandroidapp.di.ViewModelKey
 import com.anikinkirill.powerfulandroidapp.ui.main.account.AccountViewModel
+import com.anikinkirill.powerfulandroidapp.ui.main.blog.BlogViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -14,5 +15,10 @@ abstract class MainViewModelModule {
     @IntoMap
     @ViewModelKey(AccountViewModel::class)
     abstract fun bindAccountViewModel(accountViewModel: AccountViewModel) : ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(BlogViewModel::class)
+    abstract fun bindBlogViewModel(blogViewModel: BlogViewModel) : ViewModel
 
 }
