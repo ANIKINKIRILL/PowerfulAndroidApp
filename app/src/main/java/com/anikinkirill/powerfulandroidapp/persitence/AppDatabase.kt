130f94951be53376c8ece2041218201601408fb9
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-12 00:48:28 +0300

    Add BlogPostDao to a AppDatabase

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/AppDatabase.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/AppDatabase.kt
@@ -4,13 +4,16 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.anikinkirill.powerfulandroidapp.models.AccountProperties
 import com.anikinkirill.powerfulandroidapp.models.AuthToken
+import com.anikinkirill.powerfulandroidapp.models.BlogPost
 
-@Database(entities = [AccountProperties::class, AuthToken::class], version = 1)
+@Database(entities = [AccountProperties::class, AuthToken::class, BlogPost::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun getAuthTokenDao(): AuthTokenDao
 
     abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+    abstract fun getBlogPostDao(): BlogPostDao
 
     companion object {
         const val DATABASE_NAME = "app_db"
