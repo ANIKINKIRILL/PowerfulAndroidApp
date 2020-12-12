bee21fbd63e23b8c93d88509b701641486f6f371
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-12 15:58:35 +0300

    Add get all blog posts from db

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/BlogPostDao.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/BlogPostDao.kt
@@ -1,8 +1,10 @@
 package com.anikinkirill.powerfulandroidapp.persitence
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.anikinkirill.powerfulandroidapp.models.BlogPost
 
 @Dao
@@ -10,5 +12,8 @@ interface BlogPostDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(blogPost: BlogPost): Long
+
+    @Query("SELECT * FROM blog_post")
+    fun getAllBlogPosts(): LiveData<List<BlogPost>>
 
 }
