f05b3a3a656e31829e62f7fc854f1c9b021195bc
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-27 01:38:05 +0300

    Create searchBlogPost method in BlogRepository

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/repository/main/BlogRepository.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/repository/main/BlogRepository.kt
@@ -1,9 +1,24 @@
 package com.anikinkirill.powerfulandroidapp.repository.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.switchMap
 import com.anikinkirill.powerfulandroidapp.api.main.OpenApiMainService
+import com.anikinkirill.powerfulandroidapp.api.main.responses.BlogListSearchResponse
+import com.anikinkirill.powerfulandroidapp.models.AuthToken
+import com.anikinkirill.powerfulandroidapp.models.BlogPost
 import com.anikinkirill.powerfulandroidapp.persitence.BlogPostDao
 import com.anikinkirill.powerfulandroidapp.repository.JobManager
+import com.anikinkirill.powerfulandroidapp.repository.NetworkBoundResource
 import com.anikinkirill.powerfulandroidapp.session.SessionManager
+import com.anikinkirill.powerfulandroidapp.ui.DataState
+import com.anikinkirill.powerfulandroidapp.ui.main.blog.state.BlogViewState
+import com.anikinkirill.powerfulandroidapp.util.ApiResponse
+import com.anikinkirill.powerfulandroidapp.util.DateUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class BlogRepository
@@ -15,6 +30,82 @@ class BlogRepository
 
     companion object {
         private const val TAG = "AppDebug_BlogRepository"
+    }
+
+    fun searchBlogPosts(authToken: AuthToken, query: String): LiveData<DataState<BlogViewState>> {
+        return object : NetworkBoundResource<BlogListSearchResponse, List<BlogPost>, BlogViewState>(
+            sessionManager.isConnectedToTheInternet(),
+            true,
+            false,
+            true
+        ) {
+            override suspend fun createCacheRequestAndReturn() {
+                withContext(Dispatchers.Main) {
+                    result.addSource(loadFromCache()) {
+                        onCompleteJob(DataState.data(it, null))
+                    }
+                }
+            }
+
+            override suspend fun handleApiSuccessResponse(response: ApiResponse.ApiSuccessResponse<BlogListSearchResponse>) {
+                val blogPostList = response.body.results.map {
+                    BlogPost(
+                        it.pk,
+                        it.title,
+                        it.slug,
+                        it.body,
+                        it.image,
+                        DateUtils.convertServerStringDateToLong(it.date_updated),
+                        it.username
+                    )
+                }
+                updateLocalDb(blogPostList)
+                createCacheRequestAndReturn()
+            }
+
+            override fun createCall(): LiveData<ApiResponse<BlogListSearchResponse>> {
+                return openApiMainService.searchListBlogPosts(
+                    "Token ${authToken.token}",
+                    query
+                )
+            }
+
+            override fun setJob(job: Job) {
+                addJob("searchBlogPosts", job)
+            }
+
+            override fun loadFromCache(): LiveData<BlogViewState> {
+                return blogPostDao.getAllBlogPosts()
+                    .switchMap {
+                        object : LiveData<BlogViewState>() {
+                            override fun onActive() {
+                                super.onActive()
+                                value = BlogViewState(
+                                    blogFields = BlogViewState.BlogFields(
+                                        blogList = it
+                                    )
+                                )
+                            }
+                        }
+                    }
+            }
+
+            override suspend fun updateLocalDb(cacheObject: List<BlogPost>?) {
+                cacheObject?.forEach {
+                    withContext(Dispatchers.IO) {
+                        try {
+                            // inserting blog parallel
+                            launch {
+                                Log.d(TAG, "updateLocalDb: inserting a blog: $it")
+                                blogPostDao.insert(it)
+                            }
+                        } catch (e: Exception) {
+                            Log.e(TAG, "updateLocalDb: error updating cache with slug: ${it.slug}")
+                        }
+                    }
+                }
+            }
+        }.asLiveData()
     }
 
 }
