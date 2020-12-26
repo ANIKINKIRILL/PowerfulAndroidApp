eac0b16e0c4b296cb58f95931f255e84d255288d
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-27 02:07:19 +0300

    Subscribe observers in BlogFragment

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogFragment.kt
@@ -1,14 +1,21 @@
 package com.anikinkirill.powerfulandroidapp.ui.main.blog
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.anikinkirill.powerfulandroidapp.R
+import com.anikinkirill.powerfulandroidapp.ui.main.blog.state.BlogStateEvent.BlogSearchEvent
 import kotlinx.android.synthetic.main.fragment_blog.*
 
 class BlogFragment : BaseBlogFragment() {
+
+    companion object {
+        private const val TAG = "AppDebug_BlogFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +32,31 @@ class BlogFragment : BaseBlogFragment() {
             findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
         }
 
+        subscribeObservers()
+        executeSearch()
     }
 
+    private fun executeSearch() {
+        viewModel.setQuery("")
+        viewModel.setStateEvent(BlogSearchEvent())
+    }
+
+    private fun subscribeObservers() {
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
+            dataState?.let {
+                onDataStateChangeListener.onDataStateChange(it)
+                it.data?.let { data ->
+                    data.data?.let { event ->
+                        event.getContentIfNotHandled()?.let { viewState ->
+                            viewModel.setBlogPostList(viewState.blogFields.blogList)
+                        }
+                    }
+                }
+            }
+        })
+
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
+            Log.d(TAG, "BlogFragment, ViewState: $viewState")
+        })
+    }
 }
