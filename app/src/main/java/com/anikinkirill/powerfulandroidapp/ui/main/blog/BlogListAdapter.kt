526cff67af3383c2d74fef7769c9884f16e0eb29
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-27 22:02:35 +0300

    Fix bug with submitList

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogListAdapter.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/BlogListAdapter.kt
@@ -103,7 +103,7 @@ class BlogListAdapter(
         if (isQueryExhausted) {
             newList?.add(NO_MORE_RESULTS_BLOG_MARKER)
         }
-        differ.submitList(list)
+        differ.submitList(newList)
     }
 
     class BlogViewHolder(
