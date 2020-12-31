aba7e74d35a2f2f317462191f25761ee656b905d
ANIKINKIRILL <ky.anikin@mail.ru>
2020-12-31 15:07:06 +0300

    Subscribe observers in ViewBlogFragment.kt

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/ViewBlogFragment.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/ui/main/blog/ViewBlogFragment.kt
@@ -2,8 +2,12 @@ package com.anikinkirill.powerfulandroidapp.ui.main.blog
 
 import android.os.Bundle
 import android.view.*
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.anikinkirill.powerfulandroidapp.R
+import com.anikinkirill.powerfulandroidapp.models.BlogPost
+import com.anikinkirill.powerfulandroidapp.util.DateUtils
+import kotlinx.android.synthetic.main.fragment_view_blog.*
 
 class ViewBlogFragment : BaseBlogFragment() {
 
@@ -18,6 +22,29 @@ class ViewBlogFragment : BaseBlogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+        viewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
+            onDataStateChangeListener.onDataStateChange(dataState)
+        })
+
+        viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
+            viewState.viewBlogFields.blogPost?.let { blogPost ->
+                setBlogPostProperties(blogPost)
+            }
+        })
+    }
+
+    private fun setBlogPostProperties(blogPost: BlogPost) {
+        with(blogPost) {
+            requestManager.load(image).into(blog_image)
+            blog_title.text = title
+            blog_author.text = username
+            blog_update_date.text = DateUtils.convertLongToStringDate(date_updated)
+            blog_body.text = body
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
