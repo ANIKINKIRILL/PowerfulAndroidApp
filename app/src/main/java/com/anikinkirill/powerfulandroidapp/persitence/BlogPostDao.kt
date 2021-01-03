fdea130bf4aabcbfcf30d049efadfad2a1aea095
ANIKINKIRILL <ky.anikin@mail.ru>
2021-01-03 14:21:43 +0300

    Update BlogPostDao getAllPosts
    Create multiline SQL statement

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/BlogPostDao.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/persitence/BlogPostDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.anikinkirill.powerfulandroidapp.models.BlogPost
+import com.anikinkirill.powerfulandroidapp.util.Constants.Companion.PAGINATION_PAGE_SIZE
 
 @Dao
 interface BlogPostDao {
@@ -13,7 +14,16 @@ interface BlogPostDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(blogPost: BlogPost): Long
 
-    @Query("SELECT * FROM blog_post")
-    fun getAllBlogPosts(): LiveData<List<BlogPost>>
-
+    @Query("""
+        SELECT * FROM blog_post 
+        WHERE title LIKE '%' || :query || '%' 
+        OR body LIKE '%' || :query || '%' 
+        OR username LIKE '%' || :query || '%' 
+        LIMIT (:page * :pageSize)
+    """)
+    fun getAllBlogPosts(
+        query: String,
+        page: Int,
+        pageSize: Int = PAGINATION_PAGE_SIZE
+    ): LiveData<List<BlogPost>>
 }
