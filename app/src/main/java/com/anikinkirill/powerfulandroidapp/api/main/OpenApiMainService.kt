02a89c71b359229025f136178555e306deebe6c7
ANIKINKIRILL <ky.anikin@mail.ru>
2021-02-06 19:40:45 +0300

    Add deleteBlogPost to OpenApiMainService.kt

--- app/src/main/java/com/anikinkirill/powerfulandroidapp/api/main/OpenApiMainService.kt
+++ app/src/main/java/com/anikinkirill/powerfulandroidapp/api/main/OpenApiMainService.kt
@@ -45,4 +45,9 @@ interface OpenApiMainService {
         @Path("slug") slug: String
     ): LiveData<ApiResponse<GenericResponse>>
 
+    @POST("api/blog/{slug}/delete")
+    fun deleteBlogPost(
+        @Header("Authorization") authorization: String,
+        @Path("slug") slug: String
+    ): LiveData<ApiResponse<GenericResponse>>
 }
